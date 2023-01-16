e25ed2d6bccc7f58c3ea62a41f4ee642396fe201
Miguel Lopez Pedres <lopezpedres@gmail.com>
2023-01-16 13:30:50 -0700

    Changed Image Tag

--- pages/Components/NavBar/Cart.tsx
+++ pages/Components/NavBar/Cart.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import Link from "next/link";
+import Image from "next/image";
 const Cart = () => {
   return (
     <Link href="/cart">
       <a>
         <div className="grid grid-col-1 md:hidden justify-end ">
-          <img src="/cart.svg" />
+          <Image width={28} height={28} alt={"Shopping Cart"} src="/cart.svg" />
         </div>
       </a>
     </Link>
